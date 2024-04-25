3488b95fd495a1f9a9763d91d2e5260b750495c8
Christian Collins <christian.collins1@sinclair.edu>
2024-04-25 11:00:28 -0400

    added if statement checking if value is the same as int temp. temp is equal to value, as long as value is an int. if value is an int, it divides value by two and rounds the result. if its not an int, it divides it by two but doesn't round it. it then returns result

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,16 @@ using namespace std;
 template <typename T>
 T half(T value)
 {
-
+	int temp = value;
+	double result;
+	if (value == temp)
+	{
+		result = value / 2.0;
+		result = round(result);
+	}
+	else
+		result = value / 2.0;
+	return(result);
 }
 
 int main()
